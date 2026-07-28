434016ad61398b4e3dd06223ebcd94632d0c7920
resousadev <resousadev@gmail.com>
2026-07-28 00:00:23 -0300

    initialize project structure with Gradle and Spring Boot setup

---
+++ src/main/java/resousadev/eshopmodularmonoliths/EshopModularMonolithsApplication.java
@@ -0,0 +1,13 @@
+package resousadev.eshopmodularmonoliths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopModularMonolithsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EshopModularMonolithsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/resousadev/eshopmodularmonoliths/EshopModularMonolithsApplicationTests.java
@@ -0,0 +1,13 @@
+package resousadev.eshopmodularmonoliths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopModularMonolithsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
