83c37217ca3c4df0a552f6d47f7dd3d2e0d90333
resousadev <resousadev@gmail.com>
2026-07-28 22:01:06 -0300

    refactor package structure to align with API organization

--- src/main/java/resousadev/api/EshopModularMonolithsApplication.java
+++ src/main/java/resousadev/api/EshopModularMonolithsApplication.java
@@ -1,4 +1,4 @@
-package resousadev.eshopmodularmonoliths;
+package resousadev.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/resousadev/api/EshopModularMonolithsApplicationTests.java
+++ src/test/java/resousadev/api/EshopModularMonolithsApplicationTests.java
@@ -1,4 +1,4 @@
-package resousadev.eshopmodularmonoliths;
+package resousadev.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
